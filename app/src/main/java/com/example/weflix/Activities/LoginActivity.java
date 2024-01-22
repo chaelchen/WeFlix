4ddb8209350ba83d792251c44c24167d0e8322a0
Daniel <danielwiranata20@gmail.com>
2024-01-22 22:11:53 +0700

    Register fix

--- app/src/main/java/com/example/weflix/Activities/LoginActivity.java
+++ app/src/main/java/com/example/weflix/Activities/LoginActivity.java
@@ -4,14 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.weflix.DBHelper;
 import com.example.weflix.R;
 
 public class LoginActivity extends AppCompatActivity {
-    private EditText userEdt,passEdt;
+    DBHelper dbhelper;
+    private EditText etusername,etpassword;
     private Button loginBtn, registerBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,26 +26,39 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        userEdt=findViewById(R.id.editTextUsername);
-        passEdt=findViewById(R.id.editTextPassword);
+        dbhelper = new DBHelper(this);
+        etusername=findViewById(R.id.editTextUsername);
+        etpassword=findViewById(R.id.editTextPassword);
         loginBtn=findViewById(R.id.loginBtn);
         registerBtn=findViewById(R.id.registerBtn);
 
-        loginBtn.setOnClickListener(v -> {
-            if(userEdt.getText().toString().isEmpty() || passEdt.getText().toString().isEmpty()){
-                Toast.makeText(LoginActivity.this, "Please input your username and password", Toast.LENGTH_SHORT).show();
-            }
-            else if (userEdt.getText().toString().equals("test") && passEdt.getText().toString().equals("test")) {
-                startActivity(new Intent(LoginActivity.this,MainActivity.class));
-            }
-            else {
-                Toast.makeText(LoginActivity.this, "The Username or Password you entered is incorrect", Toast.LENGTH_SHORT).show();
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean logid = dbhelper.checkuser(etusername.getText().toString(),etpassword.getText().toString());
+                if(logid){
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(LoginActivity.this,"Login Gagal", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
         registerBtn.setOnClickListener(v -> {
-           startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+
+            Log.d("LoginActivity", "Register button clicked");
+
+            startActivity(new Intent(getBaseContext(),RegisterActivity.class));
         });
+
+//        registerBtn.setOnClickListener(new View.OnClickListener(){
+//
+//            public void onClick(View v){
+//                Intent gotoactivity2 = new Intent(LoginActivity.this, RegisterActivity.class);
+//                startActivity(gotoactivity2, 0);
+//            }
+//        });
 
     }
 }
--- app/src/main/java/com/example/weflix/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/weflix/Activities/RegisterActivity.java
@@ -1,7 +1,9 @@
 package com.example.weflix.Activities;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,35 +11,67 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatEditText;
 
+import com.example.weflix.DBHelper;
 import com.example.weflix.R;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class RegisterActivity extends AppCompatActivity {
-    private Button loginBtn1, registerBtn1;
-    private EditText userRegis1,passRegis1,passRegis2;
 
+    private AppCompatEditText editTextUsername, editTextPassword, editTextConfirm;
+    private Button buttonReg,buttonLog;
+    DBHelper dbHelper;
+
+    @SuppressLint("WrongViewCast")
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        editTextUsername = findViewById(R.id.editTextUsername1);
+        editTextPassword = findViewById(R.id.editTextPassword1);
+        editTextConfirm = findViewById(R.id.editTextPassword2);
+        buttonReg = findViewById(R.id.registerBtn1);
+        buttonLog = findViewById(R.id.loginBtn1);
+        buttonLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        dbHelper = new  DBHelper(this);
 
-        initView1();
-    }
+        buttonReg.setOnClickListener(new View.OnClickListener() {
 
-    private void initView1() {
-//        userRegis1=findViewById(R.id.editTextUsername1);
-//        passRegis1=findViewById(R.id.editTextPassword1);
-//        passRegis2=findViewById(R.id.editTextPassword2);
-//
-//        registerBtn1.setOnClickListener(v -> {
-//            if(userRegis1.getText().toString().isEmpty() || passRegis1.getText().toString().isEmpty() || passRegis2.getText().toString().isEmpty()){
-//                Toast.makeText(RegisterActivity.this, "Please fill in your account information", Toast.LENGTH_SHORT).show();
-//            }
-//            else{
-//                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
-//            }
-//        });
-//
-//        loginBtn1.setOnClickListener(v -> startActivity(new Intent(RegisterActivity.this,LoginActivity.class)));
+            @Override
+            public void onClick(View view) {
+                String username, password, confirm;
+                username = String.valueOf(editTextUsername.getText());
+                password = String.valueOf(editTextPassword.getText());
+                confirm = String.valueOf(editTextConfirm.getText());
+
+                if (username.equals("") || password.equals("") || confirm.equals("")) {
+                    Toast.makeText(RegisterActivity.this, "Semua kolom Text Wajib Diisi", Toast.LENGTH_SHORT).show();
+                }else{
+                    if(password.equals(confirm)){
+                        if(dbHelper.checkusername(username)){
+                            Toast.makeText(RegisterActivity.this, "User sudah ada",Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        boolean registersukses = dbHelper.insertData(username,password);
+                        if(registersukses){
+                            Toast.makeText(RegisterActivity.this, "Berhasil Menambahkan User",Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(RegisterActivity.this, "Registrasi Gagal",Toast.LENGTH_SHORT).show();
+                        }
+                    }else {
+                        Toast.makeText(RegisterActivity.this,"Password tidak sama", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
 }
+
